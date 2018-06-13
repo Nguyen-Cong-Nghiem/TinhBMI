d8bc34c24b61598f2db5dbd8af16551d1f96e8f8
Nguyen-Cong-Nghiem <kukull.ncn@gmail.com>
2018-06-13 10:36:21 +0700

    Add file BaiBMI

--- src/BaiBMI.java
+++ src/BaiBMI.java
@@ -4,12 +4,19 @@ public class BaiBMI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double weight,height,bmi;
-        System.out.println("Your weight(in kilogram:)");
+        System.out.println("Your weight(in kilogram):");
         weight= scanner.nextDouble();
         System.out.println("Your height(in meter ):");
         height = scanner.nextDouble();
         bmi= weight/Math.pow(height,2);
-        System.out.println("%-20s%s");
+        if(bmi<18)
+            System.out.printf("%-20f%s",bmi,"Underweight");
+        else if(bmi<25)
+            System.out.printf("%-20f%s",bmi,"Normal");
+        else if(bmi<30)
+            System.out.printf("%-20f%s",bmi,"Overweight");
+        else
+            System.out.printf("%-20f%s",bmi,"Obese");
 
 
 
